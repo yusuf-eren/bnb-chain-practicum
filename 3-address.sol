b158daa00d700022add202c4688bc63ad83910f7
Yusuf Eren <yusuf.e@mindbehind.com>
2023-03-29 04:01:46 +0300

    feat: comments added

--- 3-address.sol
+++ 3-address.sol
@@ -3,6 +3,9 @@
 pragma solidity 0.8.17;
 
 contract Address {
+    // Setting `owner` variable to msg.sender
+    // address variable type is a special type
+    // for just storing address data in solidity
     address private owner = msg.sender;
 
     function getOwner() public view returns(address) {
