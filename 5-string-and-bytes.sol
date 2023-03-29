770620fa0c5929fea8d6f4caeb10ae696addc62b
Yusuf Eren <yusuf.e@mindbehind.com>
2023-03-29 03:58:18 +0300

    feat: comments added

--- 5-string-and-bytes.sol
+++ 5-string-and-bytes.sol
@@ -7,5 +7,9 @@ contract StringAndBytes {
 
     bytes public helloText = "Hello, World!";
 
+    // We are converting bytes to string
+    // It will be 0x48656c6c6f2c20576f726c6421
+    // use the site below to check conversion
+    // https://onlinestringtools.com/convert-bytes-to-string
     string public convertedText = string(helloText);
 }
