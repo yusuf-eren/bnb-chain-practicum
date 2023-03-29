9e807b7b94d06a7a9ffe6729f067b813bd3302b0
Yusuf Eren <yusuf.e@mindbehind.com>
2023-03-29 04:00:25 +0300

    feat: comments added

--- 2-boolean.sol
+++ 2-boolean.sol
@@ -9,6 +9,9 @@ contract Booleans {
         return isOwner;
     }  
 
+    // Setting a new value to the `isOnwer` variable
+    // There is no returning data so we don't need to put 'returns'
+    // at the end of the function
     function setVar(bool newValue) public {
         isOwner = newValue;
     }  
