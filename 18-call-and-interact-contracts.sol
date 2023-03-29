2fc582ddaab2b22d5dbad6c8698ddb32017af54e
Yusuf Eren <yusuf.e@mindbehind.com>
2023-03-30 02:35:30 +0300

    feat: calling contracts and interacting with them

---
+++ 18-call-and-interact-contracts.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+// HOW TO TEST THAT?
+
+// 1) Deploy `ContractA` and set a value.
+// 2) Deploy `ContractB`
+// 3) Copy the address of the deployed `ContractA`
+// 4) Paste the address to `setValueOnContractA` and set your number
+// 5) check `ContractA` `someValue` variable. It must be changed.
+
+contract ContractA {
+    uint256 public someValue;
+
+    function setValue(uint256 newValue) external {
+        someValue = newValue;
+    }
+}
+
+contract ContractB {
+    function setValueOnContractA(address _contractA, uint256 newValue) public {
+        _contractA.call(abi.encodeWithSignature("setValue(uint256)", newValue));
+    }
+}
