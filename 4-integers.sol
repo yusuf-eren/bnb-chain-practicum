f1eaeb3fc1b1d30255d1a733a946236d69bc43b6
Yusuf Eren <yusuf.e@mindbehind.com>
2023-03-29 04:03:38 +0300

    feat: comments

--- 4-integers.sol
+++ 4-integers.sol
@@ -3,6 +3,10 @@
 pragma solidity 0.8.17;
 
 contract Integers {
+    // unsigned integers (uint) are starting from 0
+    // Difference between int and uint is the range
+    // of the integer value. int can get values below 0
+    // but uint can't.
     uint public number;
 
     int public anotherNumber;
